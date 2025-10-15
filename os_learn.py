a7ffbd5c285bdc3f08520e50b7c608e2e8184085
LIOR-YAACOV <LIORBE111110@GMAIL.COM>
2025-10-15 17:10:02 +0300

    added prints between start and join

--- os_learn.py
+++ os_learn.py
@@ -5,15 +5,20 @@ import multiprocessing
 def secondary(target_number):
     for i in range(target_number):
         print(i)
+        time.sleep(1)
     print(f"Secondary pid is {os.getpid()} and the ppid is {os.getppid()}")
 
 def main():
     print(f"Current pid is {os.getpid()} and the ppid is {os.getppid()}")
     process = multiprocessing.Process(target=secondary, args=(10,))
     process.start()
+    print("Main process continues to run while the secondary process is running.")
+    print_starting_time = time.time()
+    for i in range(4000):
+        print(f"Main process iteration {i}")
+    print_ending_time = time.time()
+    print(f"Main process completed in {print_ending_time - print_starting_time} seconds.")
     process.join()
-    print(1)
-    print(2)
     
     # time.sleep(20)
 
